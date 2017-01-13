e43520da6d5c1811766737703070dcab2a0ae8a0
alexsedova <als@praqma.net>
2017-01-13 20:25:11 +0100

    Update organization class

--- lib/github_wrapper.py
+++ lib/github_wrapper.py
@@ -10,10 +10,14 @@ GITHUB_INSTANCE = Github(token)
 def add_new_user_to_organization(organization= "sysdevprosup", username="carmosin"):
     org = GITHUB_INSTANCE.get_organization(organization)
     new_user = GITHUB_INSTANCE.get_user(username)
+    #team = "develop"
+
     print "USER: ", new_user
+    #url = "{}/members".format(org.url)
     url = "{}/memberships/{}".format(org.url, username)
     print "URL: ", url
-    req = requests.post(url)
+
+    req = requests.put(url, headers={'Authorization': 'token %s'%token})
     print req
     members = org.get_members()
 
--- lib/organisation.py
+++ lib/organisation.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python
 
 """
-Data class to hold info about organisations in Praqma
+Data class to hold info about organizations in Praqma
 goid - Google Suite id
 ghoid - GitHub org id
 """
+import os
+import requests
+from github import Github
 
-class Organisation:
+
+class Organization:
+    token = os.environ["GITHUB_TOKEN"]
+    gh = Github(token)
+
     def __init__(self, goid, ghoid):
         self.goid = goid
         self.ghoid = ghoid
+
+    def add_new_user_to_organization(self, organization, username):
+        org = self.gh.get_organization(organization)
+        url = "{}/memberships/{}".format(org.url, username)
+        req = requests.put(url, headers={'Authorization': 'token %s' % self.token})
