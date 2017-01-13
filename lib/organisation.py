8ebdc5aafe004a86c8fb0816df337c1792806855
alexsedova <als@praqma.net>
2017-01-13 21:31:53 +0100

    Update

--- lib/organisation.py
+++ lib/organisation.py
@@ -10,15 +10,15 @@ import requests
 from github import Github
 
 
-class Organization:
+class Organisation:
     token = os.environ["GITHUB_TOKEN"]
     gh = Github(token)
 
     def __init__(self, goid, ghoid):
         self.goid = goid
         self.ghoid = ghoid
-    def add_new_user_to_organization(self, organization, username):
-        org = self.gh.get_organization(organization)
+    def add_new_user_to_github_organization(self, ghoid, username):
+        org = self.gh.get_organization(ghoid)
         url = "{}/memberships/{}".format(org.url, username)
         req = requests.put(url, headers={'Authorization': 'token %s' % self.token})
     def __repr__(self):
